90899c1598d1282b971f5faec6de092c1329c692
Amponsem Kwabena <kwabenaamponsem@gmail.com>
2021-03-05 00:48:45 +0100

    add switches to profiler

--- Original/run_test.sh
+++ Original/run_test.sh
@@ -4,12 +4,17 @@ make
 
 INPUT_DIR=images/original
 OUTPUT_DIR=images/processed
+
+# Counter for helping profiling
+COUNTER=1
+
 mkdir $OUTPUT_DIR 2>/dev/null
 
 for i in $INPUT_DIR/*gif ; do
-    echo "==========================="
-    DEST=$OUTPUT_DIR/`basename $i .gif`-sobel.gif
-    echo "Running test on $i -> $DEST"
+	echo "==========================="
+	DEST=$OUTPUT_DIR/`basename $i .gif`-sobel.gif
+	echo "Running test on $i -> $DEST"
 
-    ./sobelf $i $DEST
+	./sobelf $i $DEST $COUNTER
+	COUNTER=$((COUNTER+1))
 done
