37a6335cfb178388744b0c2727323e67a644772b
Amponsem Kwabena <kwabenaamponsem@gmail.com>
2021-03-09 15:16:05 +0100

    add rm eval to clean_test

--- Parallelized/clean_test.sh
+++ Parallelized/clean_test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "Cleaning directory images/processes/..."
+echo "Cleaning directory eval"
 
+EVAL="eval"
+
+if [ -d "$EVAL" ];
+then
+	rm -rf $EVAL
+fi
 rm -f images/processed/*.gif
 
